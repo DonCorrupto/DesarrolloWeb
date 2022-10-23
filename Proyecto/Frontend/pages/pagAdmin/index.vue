59d1b906fcd6d806b8f1e5a83e39c80f12f3b1ad
J_Alzate <georgealzate18@hotmail.com>
2022-10-22 23:23:26 -0500

    Creacion de la Pagina del Administrador

---
+++ Proyecto/Frontend/pages/pagAdmin/index.vue
@@ -0,0 +1,170 @@
+<template>
+  <div>
+    <center>
+      <h1>Panel de Administrador</h1>
+    </center>
+    <br />
+    <div class="accordion" role="tablist">
+      <b-card no-body class="mb-1">
+        <b-card-header header-tag="header" class="p-1" role="tab">
+          <b-button
+            block
+            v-b-toggle.accordion-1
+            variant="light"
+            style="height: 100px"
+            ><h3>Destinos</h3></b-button
+          >
+        </b-card-header>
+        <b-collapse
+          id="accordion-1"
+          visible
+          accordion="my-accordion"
+          role="tabpanel"
+        >
+          <b-card-body>
+            <b-row>
+              <b-col>
+                <b-card-group deck>
+                  <b-card
+                    header="#Pais"
+                    header-text-variant="white"
+                    header-tag="header"
+                    header-bg-variant="dark"
+                    footer="#button borrar "
+                    footer-tag="footer"
+                    footer-bg-variant="success"
+                    footer-border-variant="dark"
+                    title="#Ciudad"
+                    style="max-width: 20rem"
+                  >
+                    <b-card-text>#Button de subir o borrar una ciudad</b-card-text>
+                  </b-card>
+                </b-card-group>
+              </b-col>
+            </b-row>
+          </b-card-body>
+        </b-collapse>
+      </b-card>
+    </div>
+    <br />
+    <div class="accordion" role="tablist">
+      <b-card no-body class="mb-1">
+        <b-card-header header-tag="header" class="p-1" role="tab">
+          <b-button
+            block
+            v-b-toggle.accordion-2
+            variant="light"
+            style="height: 100px"
+            ><h3>Hoteles</h3></b-button
+          >
+        </b-card-header>
+        <b-collapse
+          id="accordion-2"
+          visible
+          accordion="my-accordion"
+          role="tabpanel"
+        >
+          <b-card-body>
+            <b-row>
+              <b-col>
+                <b-card-group deck>
+                  <b-card
+                    header="#Hotel"
+                    header-text-variant="white"
+                    header-tag="header"
+                    header-bg-variant="dark"
+                    footer="#button borrar "
+                    footer-tag="footer"
+                    footer-bg-variant="success"
+                    footer-border-variant="dark"
+                    title="#Ciudad"
+                    style="max-width: 20rem"
+                  >
+                  </b-card>
+                </b-card-group>
+              </b-col>
+            </b-row>
+          </b-card-body>
+        </b-collapse>
+      </b-card>
+    </div>
+    <br />
+    <div class="accordion" role="tablist">
+      <b-card no-body class="mb-1">
+        <b-card-header header-tag="header" class="p-1" role="tab">
+          <b-button
+            block
+            v-b-toggle.accordion-3
+            variant="light"
+            style="height: 100px"
+            ><h3>Actividades</h3></b-button
+          >
+        </b-card-header>
+        <b-collapse
+          id="accordion-3"
+          visible
+          accordion="my-accordion"
+          role="tabpanel"
+        >
+          <b-card-body>
+            <b-row>
+              <b-col>
+                <b-card-group deck>
+                  <b-card
+                    header="#Ciudad"
+                    header-text-variant="white"
+                    header-tag="header"
+                    header-bg-variant="dark"
+                    footer="#button borrar "
+                    footer-tag="footer"
+                    footer-bg-variant="success"
+                    footer-border-variant="dark"
+                    title="#Actividad"
+                    style="max-width: 20rem"
+                  >
+                  </b-card>
+                </b-card-group>
+              </b-col>
+            </b-row>
+          </b-card-body>
+        </b-collapse>
+      </b-card>
+    </div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import swal from "sweetalert";
+export default {
+  layout: "header_principal",
+  data() {
+    return {
+      text: `
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+            richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
+            brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
+            tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
+            assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
+            wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher
+            vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic
+            synth nesciunt you probably haven't heard of them accusamus labore VHS.
+        `,
+    };
+  },
+
+  beforeMount() {},
+
+  methods: {
+    async passBorrado() {
+      swal("Good job!", "You clicked the button!", "success");
+    },
+  },
+};
+</script>
+
+<style>
+.margen {
+  margin: 1%;
+}
+</style>
--- Proyecto/Frontend/pages/reserva/index.vue
+++ Proyecto/Frontend/pages/reserva/index.vue
@@ -24,55 +24,35 @@
           <b-card-body>
             <b-row>
               <b-col>
-                <v-card class="mx-auto" max-width="344" outlined>
-                  <v-list-item three-line>
-                    <v-list-item-content>
-                      <div class="text-overline mb-4">OVERLINE</div>
-                      <v-list-item-title class="text-h5 mb-1">
-                        Headline 5
-                      </v-list-item-title>
-                      <v-list-item-subtitle
-                        >Greyhound divisely hello coldly
-                        fonwderfully</v-list-item-subtitle
-                      >
-                    </v-list-item-content>
-
-                    <v-list-item-avatar
-                      tile
-                      size="80"
-                      color="grey"
-                    ></v-list-item-avatar>
-                  </v-list-item>
-
-                  <v-card-actions>
-                    <v-btn outlined rounded text> Borrar </v-btn>
-                  </v-card-actions>
-                </v-card>
-                <br />
-                <v-card class="mx-auto" max-width="344" outlined>
-                  <v-list-item three-line>
-                    <v-list-item-content>
-                      <div class="text-overline mb-4">OVERLINE</div>
-                      <v-list-item-title class="text-h5 mb-1">
-                        Headline 5
-                      </v-list-item-title>
-                      <v-list-item-subtitle
-                        >Greyhound divisely hello coldly
-                        fonwderfully</v-list-item-subtitle
-                      >
-                    </v-list-item-content>
-
-                    <v-list-item-avatar
-                      tile
-                      size="80"
-                      color="grey"
-                    ></v-list-item-avatar>
-                  </v-list-item>
-
-                  <v-card-actions>
-                    <v-btn outlined rounded text> Borrar </v-btn>
-                  </v-card-actions>
-                </v-card>
+                <b-card-group deck>
+                  <b-card
+                    no-body
+                    class="overflow-hidden"
+                    style="max-width: 540px"
+                  >
+                    <b-row no-gutters>
+                      <b-col md="6">
+                        <b-card-img
+                          src="https://picsum.photos/400/400/?image=20"
+                          alt="Image"
+                          class="rounded-0"
+                        ></b-card-img>
+                      </b-col>
+                      <b-col md="6">
+                        <b-card-body title="#Pais">
+                          <b-card-text>
+                            <p>#Ciudades</p>
+                            <p>#Ciudades</p>
+                            <p>#Ciudades</p>
+                          </b-card-text>
+                          <b-button variant="danger">
+                            #Button Borrar
+                          </b-button>
+                        </b-card-body>
+                      </b-col>
+                    </b-row>
+                  </b-card>
+                </b-card-group>
               </b-col>
             </b-row>
           </b-card-body>
@@ -93,55 +73,33 @@
           <b-card-body>
             <b-row>
               <b-col>
-                <v-card class="mx-auto" max-width="344" outlined>
-                  <v-list-item three-line>
-                    <v-list-item-content>
-                      <div class="text-overline mb-4">OVERLINE</div>
-                      <v-list-item-title class="text-h5 mb-1">
-                        Headline 5
-                      </v-list-item-title>
-                      <v-list-item-subtitle
-                        >Greyhound divisely hello coldly
-                        fonwderfully</v-list-item-subtitle
-                      >
-                    </v-list-item-content>
-
-                    <v-list-item-avatar
-                      tile
-                      size="80"
-                      color="grey"
-                    ></v-list-item-avatar>
-                  </v-list-item>
-
-                  <v-card-actions>
-                    <v-btn outlined rounded text> Borrar </v-btn>
-                  </v-card-actions>
-                </v-card>
-                <br />
-                <v-card class="mx-auto" max-width="344" outlined>
-                  <v-list-item three-line>
-                    <v-list-item-content>
-                      <div class="text-overline mb-4">OVERLINE</div>
-                      <v-list-item-title class="text-h5 mb-1">
-                        Headline 5
-                      </v-list-item-title>
-                      <v-list-item-subtitle
-                        >Greyhound divisely hello coldly
-                        fonwderfully</v-list-item-subtitle
-                      >
-                    </v-list-item-content>
-
-                    <v-list-item-avatar
-                      tile
-                      size="80"
-                      color="grey"
-                    ></v-list-item-avatar>
-                  </v-list-item>
-
-                  <v-card-actions>
-                    <v-btn outlined rounded text> Borrar </v-btn>
-                  </v-card-actions>
-                </v-card>
+                <b-card-group deck>
+                  <b-card
+                    no-body
+                    class="overflow-hidden"
+                    style="max-width: 540px"
+                  >
+                    <b-row no-gutters>
+                      <b-col md="6">
+                        <b-card-img
+                          src="https://picsum.photos/400/400/?image=20"
+                          alt="Image"
+                          class="rounded-0"
+                        ></b-card-img>
+                      </b-col>
+                      <b-col md="6">
+                        <b-card-body title="#Hotel">
+                          <b-card-text>
+                            <h6>#Ciudad</h6>
+                          </b-card-text>
+                          <b-button variant="danger">
+                            #Button Borrar
+                          </b-button>
+                        </b-card-body>
+                      </b-col>
+                    </b-row>
+                  </b-card>
+                </b-card-group>
               </b-col>
             </b-row>
           </b-card-body>
@@ -162,57 +120,35 @@
           <b-card-body>
             <b-row>
               <b-col>
-                <v-card class="mx-auto" max-width="344" outlined>
-                  <v-list-item three-line>
-                    <v-list-item-content>
-                      <div class="text-overline mb-4">OVERLINE</div>
-                      <v-list-item-title class="text-h5 mb-1">
-                        Headline 5
-                      </v-list-item-title>
-                      <v-list-item-subtitle
-                        >Greyhound divisely hello coldly
-                        fonwderfully</v-list-item-subtitle
-                      >
-                    </v-list-item-content>
-
-                    <v-list-item-avatar
-                      tile
-                      size="80"
-                      color="grey"
-                    ></v-list-item-avatar>
-                  </v-list-item>
-
-                  <v-card-actions>
-                    <v-btn outlined rounded text> Borrar </v-btn>
-                  </v-card-actions>
-                </v-card>
-                <br />
-                <v-card class="mx-auto" max-width="344" outlined>
-                  <v-list-item three-line>
-                    <v-list-item-content>
-                      <div class="text-overline mb-4">OVERLINE</div>
-                      <v-list-item-title class="text-h5 mb-1">
-                        Headline 5
-                      </v-list-item-title>
-                      <v-list-item-subtitle
-                        >Greyhound divisely hello coldly
-                        fonwderfully</v-list-item-subtitle
-                      >
-                    </v-list-item-content>
-
-                    <v-list-item-avatar
-                      tile
-                      size="80"
-                      color="grey"
-                    ></v-list-item-avatar>
-                  </v-list-item>
-
-                  <v-card-actions>
-                    <v-btn outlined rounded text> Borrar </v-btn>
-                  </v-card-actions>
-                </v-card>
+                <b-card-group deck>
+                  <b-card
+                    no-body
+                    class="overflow-hidden"
+                    style="max-width: 540px"
+                  >
+                    <b-row no-gutters>
+                      <b-col md="6">
+                        <b-card-img
+                          src="https://picsum.photos/400/400/?image=20"
+                          alt="Image"
+                          class="rounded-0"
+                        ></b-card-img>
+                      </b-col>
+                      <b-col md="6">
+                        <b-card-body title="#Actividad">
+                          <b-card-text>
+                            <h6>#Pais</h6>
+                          </b-card-text>
+                          <b-button variant="danger">
+                            #Button Borrar
+                          </b-button>
+                        </b-card-body>
+                      </b-col>
+                    </b-row>
+                  </b-card>
+                </b-card-group>
               </b-col>
-            </b-row>
+            </b-row>z
           </b-card-body>
         </b-collapse>
       </b-card>
@@ -242,32 +178,16 @@ export default {
 
   beforeMount() {},
 
-  methods: {},
+  methods: {
+    async passBorrado() {
+      swal("Good job!", "You clicked the button!", "success");
+    },
+  },
 };
 </script>
 
 <style>
 .margen {
   margin: 1%;
-}
-.btnDestinos {
-  border: 3px solid rgba(100, 100, 100, 0.1);
-  margin-bottom: 20px;
-  padding: 20px;
-  width: 300px;
-  font-family: "Poppins", sans-serif;
-  font-size: 16px;
-  border-radius: 8px;
-  cursor: pointer;
-  color: #fff;
-  background-size: 300%;
-}
-
-.btn:hover {
-  background-position: right;
-}
-
-.btn1 {
-  background-image: linear-gradient(to left, #00b7da, #161569, #00b7da);
 }
 </style>
