06b213849b9bce2a158204a4f074860c74ca1865
J_Alzate <georgealzate18@hotmail.com>
2022-11-15 13:02:38 -0500

    Mensajes de error (Hoteles) API

--- Proyecto/Backend/src/routes/hoteles.js
+++ Proyecto/Backend/src/routes/hoteles.js
@@ -1,51 +1,90 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const hotelSchema = require('../models/hoteles')
+const hotelSchema = require("../models/hoteles");
 
 // create user
-router.post('/hoteles', (req, res) => {
-    const hotel = hotelSchema(req.body)
+router.post("/hoteles", (req, res) => {
+  try {
+    const hotel = hotelSchema(req.body);
     hotel
-        .save()
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }))
-})
+      .save()
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  } catch (error) {
+    res.status(500).send({
+      ok: false,
+      message: "Error",
+      info: error.toString(),
+    });
+  }
+});
 
 //get all hotel
-router.get('/hoteles', (req, res) => {
+router.get("/hoteles", (req, res) => {
+  try {
     hotelSchema
-        .find()
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }))
-})
+      .find()
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  } catch (error) {
+    res.status(500).send({
+      ok: false,
+      message: "Error",
+      info: error.toString(),
+    });
+  }
+});
 
 // get a hotel
-router.get('/hoteles/:id', (req, res) => {
+router.get("/hoteles/:id", (req, res) => {
+  try {
     const { id } = req.params;
     hotelSchema
-        .findById(id)
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }))
-})
+      .findById(id)
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  } catch (error) {
+    res.status(500).send({
+      ok: false,
+      message: "Error",
+      info: error.toString(),
+    });
+  }
+});
 
 //update a hotel
-router.put('/hoteles/:id', (req, res) => {
+router.put("/hoteles/:id", (req, res) => {
+  try {
     const { id } = req.params;
     const { pais, imgPais, ciudad } = req.body;
     hotelSchema
-        .updateOne({ _id:id }, { $set:{pais, imgPais, ciudad} })
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }))
-})
+      .updateOne({ _id: id }, { $set: { pais, imgPais, ciudad } })
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  } catch (error) {
+    res.status(500).send({
+      ok: false,
+      message: "Error",
+      info: error.toString(),
+    });
+  }
+});
 
 //delete a hotel
-router.delete('/hoteles/:id', (req, res) => {
+router.delete("/hoteles/:id", (req, res) => {
+  try {
     const { id } = req.params;
     hotelSchema
-        .remove({ _id: id })
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }))
-})
-
+      .remove({ _id: id })
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  } catch (error) {
+    res.status(500).send({
+      ok: false,
+      message: "Error",
+      info: error.toString(),
+    });
+  }
+});
 
 module.exports = router;
