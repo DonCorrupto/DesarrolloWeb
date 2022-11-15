9ae4a2ae7cdbb3c459220634e17fe3591ead8b41
J_Alzate <georgealzate18@hotmail.com>
2022-11-15 13:08:14 -0500

    Mensajes de error (Paises) API

--- Proyecto/Backend/src/routes/paises.js
+++ Proyecto/Backend/src/routes/paises.js
@@ -1,51 +1,90 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const paisSchema = require('../models/paises')
+const paisSchema = require("../models/paises");
 
 // create pais
-router.post('/paises', async (req, res) => {
-    const pais = paisSchema(req.body)
+router.post("/paises", async (req, res) => {
+  try {
+    const pais = paisSchema(req.body);
     pais
-        .save()
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }))
-})
+      .save()
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  } catch (error) {
+    res.status(500).send({
+      ok: false,
+      message: "Error",
+      info: error.toString(),
+    });
+  }
+});
 
 //get all paiss
-router.get('/paises', (req, res) => {
+router.get("/paises", (req, res) => {
+  try {
     paisSchema
-        .find()
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }))
-})
+      .find()
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  } catch (error) {
+    res.status(500).send({
+      ok: false,
+      message: "Error",
+      info: error.toString(),
+    });
+  }
+});
 
 // get a pais
-router.get('/paises/:id', (req, res) => {
+router.get("/paises/:id", (req, res) => {
+  try {
     const { id } = req.params;
     paisSchema
-        .findById(id)
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }))
-})
+      .findById(id)
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  } catch (error) {
+    res.status(500).send({
+      ok: false,
+      message: "Error",
+      info: error.toString(),
+    });
+  }
+});
 
 //update a pais
-router.put('/paises/:id', (req, res) => {
+router.put("/paises/:id", (req, res) => {
+  try {
     const { id } = req.params;
     const { name, email, password } = req.body;
     paisSchema
-        .updateOne({ _id:id }, { $set:{name, email, password} })
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }))
-})
+      .updateOne({ _id: id }, { $set: { name, email, password } })
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  } catch (error) {
+    res.status(500).send({
+      ok: false,
+      message: "Error",
+      info: error.toString(),
+    });
+  }
+});
 
 //delete a pais
-router.delete('/paises/:id', (req, res) => {
+router.delete("/paises/:id", (req, res) => {
+  try {
     const { id } = req.params;
     paisSchema
-        .remove({ _id: id })
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }))
-})
-
+      .remove({ _id: id })
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  } catch (error) {
+    res.status(500).send({
+      ok: false,
+      message: "Error",
+      info: error.toString(),
+    });
+  }
+});
 
 module.exports = router;
